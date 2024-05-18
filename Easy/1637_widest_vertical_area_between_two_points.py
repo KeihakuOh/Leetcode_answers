1bfae2f2d6888743b8afd57a8de22b808d88e388
KeihakuOh <w1123527817@akane.waseda.jp>
2024-05-19 00:02:22 +0900

    change 1637

--- Easy/1637_widest_vertical_area_between_two_points.py
+++ Easy/1637_widest_vertical_area_between_two_points.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        l=[]
+        l = []
         for i in range(len(points)):
             l.append(points[i][0])
         l.sort()
-        if(len(l)==0 or len(l)==1):
+        if(len(l) == 0 or len(l) == 1):
             return 0
-        c=0
-        for i in range(len(l)-1,0,-1):
-            d=l[i]-l[i-1]
-            if(c<d):
-                c=d
-        return c       
+        max = 0
+        for i in range(len(l) - 1):
+            num = l[i + 1] - l[i]
+            if num > max:
+                max = num
+        return max      
