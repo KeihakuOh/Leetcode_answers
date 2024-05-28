4168eb12e24753ab9a35e15c220cbf69cdf54bce
KeihakuOh <w1123527817@akane.waseda.jp>
2024-05-28 23:12:27 +0900

    fix 1302

--- Medium/1302_deepest_leaves_sum.py
+++ Medium/1302_deepest_leaves_sum.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: TreeNode) -> int:
         maxlevel = 0
@@ -11,9 +5,10 @@ class Solution:
 
         def dfs(node=root, level=0):
             nonlocal maxlevel, tsum
-            if not node: return
-                dfs(node.left, level+1)
-                dfs(node.right, level+1)
+            if not node:
+                return
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
             if maxlevel < level:
                 maxlevel = level
                 tsum = 0
