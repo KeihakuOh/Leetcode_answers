127d6eb16aad05546497144bc4e1c05944d670d4
KeihakuOh <w1123527817@akane.waseda.jp>
2024-10-28 17:17:45 +0900

    finish 1325

---
+++ Medium/1325_Delete_Leaves_With_a_Given_Value.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def newNodes(root):
+            if not root:
+                return None
+            root.left = newNodes(root.left)
+            root.right = newNodes(root.right)
+            print(root.left)
+            print(root.right)
+            if not root.left and not root.right and root.val == target:
+                return None
+            return root
+        root = newNodes(root)
+        return root
