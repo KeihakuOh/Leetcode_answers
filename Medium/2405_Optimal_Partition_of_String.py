f0eed753241e8aa1258724954caf270b2559c3aa
KeihakuOh <w1123527817@akane.waseda.jp>
2024-12-21 02:02:14 +0900

    finish 2405

---
+++ Medium/2405_Optimal_Partition_of_String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        seen = set() 
+        ans = 0 
+        for char in s:
+            if char in seen:
+                ans += 1
+                seen.clear()
+            seen.add(char)
+        return ans + 1
