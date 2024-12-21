c3d2e0771505a3eab68993a8a8b3a946521e639d
KeihakuOh <w1123527817@akane.waseda.jp>
2024-12-21 15:43:45 +0900

    change logic

--- Medium/2405_Optimal_Partition_of_String.py
+++ Medium/2405_Optimal_Partition_of_String.py
@@ -1,10 +1,10 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         seen = set() 
-        ans = 0 
+        ans = 1 
         for char in s:
             if char in seen:
                 ans += 1
                 seen.clear()
             seen.add(char)
-        return ans + 1
+        return ans
