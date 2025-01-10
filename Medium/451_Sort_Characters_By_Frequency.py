f6972dbeeb8e0da0de3de72cb67d7d9e1b7d7ab5
KeihakuOh <w1123527817@akane.waseda.jp>
2025-01-10 23:17:25 +0900

    finish 451

---
+++ Medium/451_Sort_Characters_By_Frequency.py
@@ -0,0 +1,6 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq = Counter(s)
+        sorted_chars = sorted(freq.keys(), key=lambda x: -freq[x])
+        result = "".join(char * freq[char] for char in sorted_chars)
+        return result
