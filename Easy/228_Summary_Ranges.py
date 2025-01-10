961f1244326a107acd6a84c893ddb542ea67d1cd
KeihakuOh <w1123527817@akane.waseda.jp>
2025-01-10 22:17:11 +0900

    finish 228

---
+++ Easy/228_Summary_Ranges.py
@@ -0,0 +1,16 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        ranges = []
+        start = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                ranges.append(self.format_range(start, nums[i - 1]))
+                start = nums[i]
+
+        ranges.append(self.format_range(start, nums[-1]))
+        return ranges
+
+    def format_range(self, start, end):
+        return str(start) if start == end else f"{start}->{end}"
