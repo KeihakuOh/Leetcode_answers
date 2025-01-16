a441a9332fb7d1cdf6534812203936ada717fd72
KeihakuOh <w1123527817@akane.waseda.jp>
2025-01-17 00:07:13 +0900

    finish 791

---
+++ Medium/791_Custom_Sort_String.py
@@ -0,0 +1,11 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        count = Counter(s)
+        result = []
+        for char in order:
+            if char in count:
+                result.append(char * count[char])
+                del count[char]
+        for char, freq in count.items():
+            result.append(char * freq)
+        return ''.join(result)
